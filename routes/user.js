8c45405e2db73d71de47baf5d93232318a43f041
bokdoll <winnerhj1109@naver.com>
2020-07-03 23:35:46 +0900

    [refactor] controller 수정 및 정리

--- routes/user.js
+++ routes/user.js
@@ -2,130 +2,24 @@ const express = require('express');
 const router = express.Router();
 const util = require('../modules/util');
 const jwt = require('../modules/jwt');
-const user = require('../controllers/user');
+const userController = require('../controllers/user');
 const adminModel = require('../models/admin');
-const encrypt = require('../modules/crypto');
 const auth = require('../middleware/auth');
 
 /**
  * 회원 가입
  */
-router.post('/signup', async (req, res) => {
-    const {
-        email,
-        pw,
-        pwCheck
-    } = req.body;
-
-    // 파라미터 확인
-    if (!email || !pw || !pwCheck) {
-        res.status(400).send(util.fail(400, "필수 정보를 입력하세요."));
-        return;
-    }
-
-    // 비밀번호와 비밀번호 확인이 같지 않을 때
-    if (pw !== pwCheck) {
-        res.status(400).send(util.fail(400, "비밀번호가 맞지 않습니다."));
-        return;
-    }
-
-    // id 중복 확인
-    try {
-        const result = await adminModel.findOne({
-            email: email
-        }, {
-            _id: 0,
-            email: 1,
-            auth: 1
-        })
-
-        //-----email이 존재하면-----//
-        if (result) {
-            if (result.auth) {
-                res.status(400).send(util.fail(400, "이미 존재하는 email입니다."))
-                return;
-            }
-        }
-    } catch (err) {
-        if (err) {
-            res.status(500).send(util.fail(500, "이메일 서버 에러"));
-            return;
-        }
-    }
-
-    // 이메일 전송
-    const token = user.sendEmail(email);
-
-    await user.signUp(email, pw, token);
-
-    res.status(200).send(util.success(200, "이메일 전송 완료"));
-});
-
+router.post('/signup', userController.signUp);
 
 /**
  * 이메일 인증
  */
-router.get("/auth", async (req, res) => {
-    const email = req.query.email;
-    const token = req.query.token;
-
-    // token 일치 시 auth를 true로 변경
-    const filter = {
-        email: email,
-        authToken : token,
-    };
-    const update = {
-        auth: true
-    };
-    const result = await adminModel.findOneAndUpdate(filter, update, {
-        new: true
-    })
-    
-    if (result === null){
-        res.status(400).send(util.fail(400, "이메일 인증에 실패하였습니다."));
-    }else{
-        await adminModel.update(filter, {$unset : {authToken : 1}});    // authToken 필드 삭제
-        res.status(200).send(util.success(200, "이메일 인증에 성공하였습니다."));
-    }
-});
+router.get("/auth", userController.authenticate);
 
 /**
  * 로그인
  */
-router.post("/signin", async (req, res) => {
-    const {
-        email,
-        pw
-    } = req.body;
+router.post("/signin", userController.signIn);
 
-    // auth가 true인지 확인하기
-    const result = await adminModel.findOne({
-        email: email
-    }, {
-        _id: 0,
-        email: 1,
-        auth: 1,
-        salt: 1,
-        password: 1
-    });
-
-    if (result === null){
-        return res.status(400).send(util.fail(400, "가입 정보가 없습니다."));
-    }
-
-    if (!result.auth) {
-        return res.status(400).send(util.fail(400, "이메일 인증을 받지 않았습니다."));
-    }
-
-    const salt = result.salt;
-    const hashed = await encrypt.encryptWithSalt(pw, salt);
-
-    if (result.password === hashed){
-        const {token, _} = await jwt.sign(result)
-        return res.status(200).send(util.success(200, "로그인 성공", {accessToken : token}));
-    } else{
-        return res.status(400).send(util.fail(400, "로그인 실패"));
-    }
-});
 
 module.exports = router;
