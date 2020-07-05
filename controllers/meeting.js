f166c472a286bfa4ad526ac5ffdc8277012568e8
EZYOON <fullmoon5110@gmail.com>
2020-07-05 14:58:00 +0900

    [update]유저정보 가져오기

--- controllers/meeting.js
+++ controllers/meeting.js
@@ -1,16 +1,18 @@
+const adminModel = require('../models/admin');
 const groupModel = require('../models/group');
 const meetingModel = require('../models/meeting');
 const util = require('../modules/util');
 const statusCode = require('../modules/statusCode');
 const resMessage = require('../modules/responseMessage');
+const async = require('pbkdf2/lib/async');
 
 module.exports = {
     /**
      * 첫 모임 생성 
      */
     createNewGroup: async (req, res) => {
+        const adminEmail = req.email;
         const {
-            admin,
             name,
             date,
             startTime,
@@ -18,7 +20,7 @@ module.exports = {
             headCount,
         } = req.body;
 
-        if (!admin || !name || !date || !startTime || !endTime || !headCount) {
+        if (!name || !date || !startTime || !endTime || !headCount) {
             return res.status(400).send(util.fail(400, '필요한 값이 없습니다.'))
         }
 
@@ -44,7 +46,11 @@ module.exports = {
         let fin_meeting = await newMeeting.save();
 
         var newGroup = new groupModel();
-        newGroup.admin = req.body.admin;
+        const admin = await adminModel.findOne({
+            email: adminEmail
+        })
+
+        newGroup.admin = admin._id;
         newGroup.meetings.push(fin_meeting._id)
 
         await newGroup.save();
@@ -98,7 +104,7 @@ module.exports = {
 
         await group.save();
 
-        return res.status(200).send(util.success(200, '새 모임 생성 성공', group));
+        return res.status(200).send(util.success(200, '이어서 모임 생성 성공', group));
 
     },
 
@@ -110,7 +116,10 @@ module.exports = {
         const meetingObject = await meetingModel.findOne({
             _id: meetingId
         })
-        return res.status(200).send(util.success(200, '모임 정보 조회 성공', meetingObject));
+        const data = {
+            meeting : meetingObject
+        }
+        return res.status(200).send(util.success(200, '모임 정보 조회 성공', data));
     },
 
     /**
@@ -155,10 +164,23 @@ module.exports = {
         return res.status(200).send(util.success(200, '모임 정보 수정 성공', meeting));
 
     },
-
-    // router.get('/list/:id/:round',meetingController.round);
-    round: async (req, res) => {
+    /**
+     * 모임 리스트 (각 그룹에서 최신 모임반환) 진행중 / 완료 & 예정 나눠서 반환
+     */
+    list : async(req,res)=>{
         const adminId = req.params.id;
+
+
+    },
+    /**
+     * 모임 리스트에서 각 회차의 정보 조회 round가 -1일때 마지막 회차와 회차 수 반환, 다른 수 일때는 해당 회차 정보 반환
+     */
+    round: async (req, res) => {
+        const adminEmail = req.email;
+        const admin = await adminModel.findOne({
+            email: adminEmail
+        })
+        const adminId = admin._id;
         const round = req.params.round;
 
         const group = await groupModel.findOne({
@@ -190,6 +212,7 @@ module.exports = {
 
             const data = {
                 meeting: {
+                    _id : meeting._id,
                     user: user,
                     feedBack: feedBack,
                     name: meeting.name,
@@ -226,6 +249,7 @@ module.exports = {
 
             const data = {
                 meeting: {
+                    _id : meeting._id,
                     user: user,
                     feedBack: feedBack,
                     name: meeting.name,
@@ -234,7 +258,6 @@ module.exports = {
                     endTime: meeting.endTime,
                     headCount: meeting.headCount,
                     image: meeting.image,
-
                     qrImg: meeting.qrImg
                 }
             }
