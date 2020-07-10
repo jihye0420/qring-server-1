3043b5f535bd2199c3c5ef93a4149092fa9e935c
kimminji122258 <kimminji122258@gmail.com>
2020-07-10 21:18:30 +0900

    [update] 배포

--- controllers/meeting.js
+++ controllers/meeting.js
@@ -23,11 +23,19 @@ module.exports = {
             feedBack
         } = req.body;
 
+
+        const parsedFeedbacks = feedBack.map((fb) => {
+            let parsedFb;
+            if (typeof fb === 'string') {
+                parsedFb = JSON.parse(fb);
+            }
+            return parsedFb;
+        })
+        console.log("피드백 질문 생성" + parsedFeedbacks);
+
         if (!name || !date || !startTime || !endTime || !late || !headCount) {
             return res.status(400).send(util.fail(400, '필요한 값이 없습니다.'))
         }
-
-        console.log("피드백" + feedBack);
 
         var newMeeting = new meetingModel();
         newMeeting.name = req.body.name
@@ -36,7 +44,11 @@ module.exports = {
         newMeeting.endTime = req.body.endTime
         newMeeting.late = req.body.late
         newMeeting.headCount = req.body.headCount
-        newMeeting.feedBack = req.body.feedBack
+        newMeeting.feedBack = parsedFeedbacks
+        //newMeeting.feedBack = req.body.feedBack
+
+        console.log("parsedFeedbacks");
+        console.log(parsedFeedbacks);
 
         const image = req.file.location;
         // data check - undefined
